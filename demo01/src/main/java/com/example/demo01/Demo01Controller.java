c7d256c554132d6c2a1e8101c4396c528628b13d
kairi1201 <24j003_kawashima_kairi@edu.kimura.ac.jp>
2025-04-16 02:07:32 +0000

    first commit

---
+++ demo01/src/main/java/com/example/demo01/Demo01Controller.java
@@ -0,0 +1,12 @@
+package com.example.demo01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Demo01Controller {
+    @RequestMapping("/")
+    String index(){
+        return "index";//index.htmlを編集して返す
+    }
+}
